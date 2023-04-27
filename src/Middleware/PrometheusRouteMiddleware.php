ddfc7082f7cbad4761f0f3c1e1d12fd99939209c
Gustavo Mendes <gustamms@hotmail.com>
2023-04-27 12:10:35 -0300

    Feat: Adding increase route

--- src/Middleware/PrometheusRouteMiddleware.php
+++ src/Middleware/PrometheusRouteMiddleware.php
@@ -6,6 +6,7 @@ use Closure;
 use Gustavomendes\PrometheusLaravel\PrometheusCollector;
 use Prometheus\Exception\MetricsRegistrationException;
 use Symfony\Component\HttpFoundation\Request;
+use Illuminate\Support\Facades\Route as RouteFacade;
 
 class PrometheusRouteMiddleware
 {
@@ -17,6 +18,7 @@ class PrometheusRouteMiddleware
     public function handle(Request $request, Closure $next)
     {
         $this->prometheusCollector = new PrometheusCollector();
+        $matchedRoute = $this->getMatchedRoute($request);
 
         $response = $next($request);
 
@@ -24,10 +26,16 @@ class PrometheusRouteMiddleware
             env('PROMETHEUS_NAMESPACE', 'app'),
             'request_made',
             'Request are made',
-            ['statusCode'],
-            [$response->getStatusCode()]
+            ['statusCode', 'uri'],
+            [$response->getStatusCode(), $matchedRoute->uri()]
         );
 
         return $response;
+    }
+
+    public function getMatchedRoute(Request $request): \Illuminate\Routing\Route
+    {
+        $routeCollection = RouteFacade::getRoutes();
+        return $routeCollection->match($request);
     }
 }
